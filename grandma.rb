03dca70349e28c7590baedbdba9fbfb6875095b8
Abel Kalum <chumbabel@gmail.com>
2020-01-22 14:05:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,20 +1,7 @@
-require_relative '../spec/spec_helper.rb'
-
-def speak-to-grandma (greeting phrase, response)
-  
-  if speak-to-grandma == 'LOVE YOU GRANDMA!'
-    return 'I LOVE YOU TOO PUMPKIN!'
-
-  elsif speak-to-grandma == 'Hi Nana, how are you?'
-    return 'HUH?! SPEAK UP, SONNY!'
-
-  elsif speak-to-grandma == 'Hi!'
-    return 'HUH?! SPEAK UP, SONNY!'
-
-  elsif speak-to-grandma == 'WHAT DID YOU EAT TODAY?'
-    return 'NO, NOT SINCE 1938!'
-
-  else speak-to-grandma == 'WHAT?'
-    return 'NO, NOT SINCE 1938!'
+def speak_to_grandma(string)
+  if string != string.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif string.upcase
+    return "NO, NOT SINCE 1938!"
   end
-end
+end 
