291fb6adff5bb90cda4587395856a0f6cee882d3
Abel Kalum <chumbabel@gmail.com>
2020-01-22 13:39:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,17 +1,15 @@
-def speak-to-grandma
-  if you say 'LOVE YOU GRANDMA!'
-    return 'I LOVE YOU TOO PUMPKIN!'
+if you say 'LOVE YOU GRANDMA!'
+  return 'I LOVE YOU TOO PUMPKIN!'
 
-  elsif you say 'Hi Nana, how are you?'
-    return 'HUH?! SPEAK UP, SONNY!'
+elsif you say 'Hi Nana, how are you?'
+  return 'HUH?! SPEAK UP, SONNY!'
 
-  elsif you say 'Hi!'
-    return 'HUH?! SPEAK UP, SONNY!'
+elsif you say 'Hi!'
+  return 'HUH?! SPEAK UP, SONNY!'
 
-  elsif you say 'WHAT DID YOU EAT TODAY?'
-    return 'NO, NOT SINCE 1938!'
+elsif you say 'WHAT DID YOU EAT TODAY?'
+  return 'NO, NOT SINCE 1938!'
 
-  else you say 'WHAT?'
-    return 'NO, NOT SINCE 1938!'
-  end
+else you say 'WHAT?'
+  return 'NO, NOT SINCE 1938!'
 end
