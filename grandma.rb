ed5bc7d72aa5a9ba8d9402e0c1514a43eec75046
Abel Kalum <chumbabel@gmail.com>
2020-01-22 13:48:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,17 +1,18 @@
-require_relative '..rspec ./spec/spec_helper.rb'
+def speak-to-grandma
+  
+  if speak-to-grandma == 'LOVE YOU GRANDMA!'
+    returns 'I LOVE YOU TOO PUMPKIN!'
 
-if you say 'LOVE YOU GRANDMA!'
-  return 'I LOVE YOU TOO PUMPKIN!'
+  elsif speak-to-grandma == 'Hi Nana, how are you?'
+    returns 'HUH?! SPEAK UP, SONNY!'
 
-elsif you say 'Hi Nana, how are you?'
-  return 'HUH?! SPEAK UP, SONNY!'
+  elsif speak-to-grandma == 'Hi!'
+    returns 'HUH?! SPEAK UP, SONNY!'
 
-elsif you say 'Hi!'
-  return 'HUH?! SPEAK UP, SONNY!'
+  elsif speak-to-grandma == 'WHAT DID YOU EAT TODAY?'
+    returns 'NO, NOT SINCE 1938!'
 
-elsif you say 'WHAT DID YOU EAT TODAY?'
-  return 'NO, NOT SINCE 1938!'
-
-else you say 'WHAT?'
-  return 'NO, NOT SINCE 1938!'
+  else speak-to-grandma == 'WHAT?'
+    returns 'NO, NOT SINCE 1938!'
+  end
 end
