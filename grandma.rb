d83761ecb9901c3815648ede69bf00659d28d50d
Abel Kalum <chumbabel@gmail.com>
2020-01-22 14:41:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     ans = 
   elsif
-    ans = 'HUH?! SPEAK UP, SONNY!'
+    ans = 'HUH?! SPEAK UP, SONNY!'"NO, NOT SINCE 1938!"
   else
     ans = 'I LOVE YOU TOO PUMPKIN!'
   end
