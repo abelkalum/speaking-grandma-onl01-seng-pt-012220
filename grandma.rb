b7cf02fbf80c854da0964a07096147f4c83902af
Abel Kalum <chumbabel@gmail.com>
2020-01-22 14:46:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ def speak_to_grandma(phrase)
     ans = 'I LOVE YOU TOO PUMPKIN!'
   elsif
     ans = 'HUH?! SPEAK UP, SONNY!'
-  
+  elsif
+    ans = 'HUH?! SPEAK UP, SONNY!'
   else
     ans = 'NO, NOT SINCE 1938!'
   end
