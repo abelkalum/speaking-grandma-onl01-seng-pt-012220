7a4e317aa202e906e6562e3bba85944157e0305b
Abel Kalum <chumbabel@gmail.com>
2020-01-22 14:29:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,5 @@
-def speak_to_grandma(string)
-  if string != string.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif string.upcase
-    return "NO, NOT SINCE 1938!"
-  end
-end 
-
-def speak_to_grandma(string)
-  if string == phrase.upcase
+def speak_to_grandma(phrase)
+  if phrase == phrase.upcase
     ans = "HUH, SPEAK UP SONNY!"
   elsif
     ans = 'I LOVE YOU TOO PUMPKIN!'
