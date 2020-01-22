78ec8e6be88eb9804bb8baa96525d22a7c5e26e6
Abel Kalum <chumbabel@gmail.com>
2020-01-22 13:31:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,17 @@
+def speak-to-grandma
+  if you say 'LOVE YOU GRANDMA!'
+    puts 'I LOVE YOU TOO PUMPKIN!'
 
+  elsif you say 'Hi Nana, how are you?'
+    puts 'HUH?! SPEAK UP, SONNY!'
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
+  elsif you say 'Hi!'
+    puts 'HUH?! SPEAK UP, SONNY!'
 
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
+  elsif you say 'WHAT DID YOU EAT TODAY?'
+    puts 'NO, NOT SINCE 1938!'
 
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+  else you say 'WHAT?'
+    puts 'NO, NOT SINCE 1938!'
+  end
+end
